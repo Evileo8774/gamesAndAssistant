aa6c949a6ce4f5eb0ef018b6b939510fb69c7b95
Leo <54958225+Evileo8774@users.noreply.github.com>
2022-02-13 14:22:27 +0100

    nav bar moving on scroll

--- js/index.js
+++ js/index.js
@@ -1,4 +1,23 @@
 $(document).ready(function(){
     $(".petit-bac").on("click", function(){window.location.href = "html/petit-bac.html";});
     $(".puissance-4").on("click", function(){window.location.href = "html/puissance-4.html";});
+
+    $(function(){
+        var nav = $("nav");
+        $(window).scroll(function(){
+            var scroll = $(window).scrollTop();
+            if(scroll >= 60){
+                nav.css({top: "0"});
+            } else {
+                nav.css({top: "50px"});
+            }
+        })
+    })
+
+
+
+
+
+
+
 });
